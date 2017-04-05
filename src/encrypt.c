30fcd026c740ccd44bc2ee2f1943401f54677034
FreeStraint <rwu4@ualberta.ca>
2017-04-04 23:20:04 -0600

    commit again

--- src/encrypt.c
+++ src/encrypt.c
@@ -42,47 +42,45 @@ JNIEXPORT void JNICALL Java_Encrypt_encrypt
   	printf("encrypt\n");
   	jbyte *v = (*env)->GetByteArrayElements(env, value, 0);
   	jbyte *k = (*env)->GetByteArrayElements(env, key, 0);
-
+    jboolean is_copy_value;
+    jboolean is_copy_key;
   	jsize vSize = (*env)->GetArrayLength(env, value);
 
-  	long* ve = (long*) v
-  	long* ke = (long*) k
+    long *a = (long *) v;
+    long *b = (long *) k;
+
   	int i = 0;
   	for(i = 0; i<vSize; i+=2){
-  		encrypt(ve, ke);
+  		encrypt(a, b);
   	}
+
+    (*env)->ReleaseByteArrayElements(env, value, v, is_copy_value);
+    (*env)->ReleaseByteArrayElements(env, key, k, is_copy_key);
+
   }
 
 
 JNIEXPORT void JNICALL Java_Encrypt_decrypt
   (JNIEnv *env, jobject thisObj, jbyteArray value, jbyteArray key){
 
-  	// jbyte *v = (*env)->GetByteArrayElements(env, value, 0);
-  	// jdouble *k = (*env)->GetDoubleArrayElements(env, key, 0);
+    jboolean is_copy_key;
+    jboolean is_copy_value;
+  	jbyte *v = (*env)->GetByteArrayElements(env, value, 0);
+  	jbyte *k = (*env)->GetByteArrayElements(env, key, 0);
 
-  	// jsize vSize = (*env)->GetArrayLength(env, value);
-  	// int i = 0;
-  	// for(i = 0; i<vSize; i+=2){
-  	// 	decrypt(v, k)
-  	// }
-  	printf("decrypt\n");
+    long *a = (long *) v;
+    long *b = (long *) k;
+
+  	jsize vSize = (*env)->GetArrayLength(env, value);
+  	int i = 0;
+  	for(i = 0; i<vSize; i+=2){
+  		decrypt(v, k)
+  	}
+    (*env)->ReleaseByteArrayElements(env, value, v, is_copy_value);
+    (*env)->ReleaseByteArrayElements(env, key, k, is_copy_key);
+  	
+    printf("decrypt\n");
   }
 
-#include <stdlib.h>
-#include <stdio.h>
-
-
-
-int main(int argc, char const *argv[])
-{
-  
-  char* v = "AD DE E2 DB B3 E2 DB B3";
-  char* k = "3A DA 75 21 DB E2 DB B3 11 B4 49 01 A5 C6 EA D4";
-  printf("Value: %s\n", v);
-  printf("Key: %s\n", k);
-
-
-  return 0;
-}
 
 
