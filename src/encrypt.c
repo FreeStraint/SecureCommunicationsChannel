e75a88a6f6f156765031b03a6ea19707ecb5b802
FreeStraint <rwu4@ualberta.ca>
2017-04-04 22:41:50 -0600

    just commit

--- src/encrypt.c
+++ src/encrypt.c
@@ -1,40 +1,8 @@
 #include <jni.h>
+#include <jni_md.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "Encrypt.h"
-
-JNIEXPORT void JNICALL Java_Encrypt_encrypt
-  (JNIEnv *env, jobject thisObj, jbyteArray value, jbyteArray key){
-
-  	printf("encrypt\n");
-  	jbyte *v = (*env)->GetByteArrayElements(env, value, 0);
-  	jbyte *k = (*env)->GetDoubleArrayElements(env, key, 0);
-
-  	jsize vSize = (*env)->GetArrayLength(env, value);
-  	
-  	int i = 0;
-  	for(i = 0; i<vSize; i+=2){
-  		encrypt(v, k);
-  	}
-  }
-
-
-JNIEXPORT void JNICALL Java_Encrypt_decrypt
-  (JNIEnv *env, jobject thisObj, jbyteArray value, jbyteArray key){
-
-  	// jbyte *v = (*env)->GetByteArrayElements(env, value, 0);
-  	// jdouble *k = (*env)->GetDoubleArrayElements(env, key, 0);
-
-  	// jsize vSize = (*env)->GetArrayLength(env, value);
-  	// int i = 0;
-  	// for(i = 0; i<vSize; i+=2){
-  	// 	decrypt(v, k)
-  	// }
-  	printf("decrypt\n");
-  }
-
-#include <stdlib.h>
-#include <stdio.h>
 
 void encrypt (long *v, long *k){
 /* TEA encryption algorithm */
@@ -67,6 +35,41 @@ unsigned long delta=0x9e3779b9l;
 	v[0] = y;
 	v[1] = z;
 }
+
+JNIEXPORT void JNICALL Java_Encrypt_encrypt
+  (JNIEnv *env, jobject thisObj, jbyteArray value, jbyteArray key){
+
+  	printf("encrypt\n");
+  	jbyte *v = (*env)->GetByteArrayElements(env, value, 0);
+  	jbyte *k = (*env)->GetDoubleArrayElements(env, key, 0);
+
+  	jsize vSize = (*env)->GetArrayLength(env, value);
+
+  	int i = 0;
+  	for(i = 0; i<vSize; i+=2){
+  		encrypt(v, k);
+  	}
+  }
+
+
+JNIEXPORT void JNICALL Java_Encrypt_decrypt
+  (JNIEnv *env, jobject thisObj, jbyteArray value, jbyteArray key){
+
+  	// jbyte *v = (*env)->GetByteArrayElements(env, value, 0);
+  	// jdouble *k = (*env)->GetDoubleArrayElements(env, key, 0);
+
+  	// jsize vSize = (*env)->GetArrayLength(env, value);
+  	// int i = 0;
+  	// for(i = 0; i<vSize; i+=2){
+  	// 	decrypt(v, k)
+  	// }
+  	printf("decrypt\n");
+  }
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
 
 int main(int argc, char const *argv[])
 {
