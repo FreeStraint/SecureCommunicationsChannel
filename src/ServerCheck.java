cc90f03fba94c8913a1d9426626cb29e393cbdb5
FreeStraint <rwu4@ualberta.ca>
2017-04-11 19:53:46 -0600

    salt and hash shadow file

--- src/ServerCheck.java
+++ src/ServerCheck.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class ServerCheck {
 	
@@ -16,7 +19,7 @@ public class ServerCheck {
 			String line;
 			while((line = br.readLine()) != null){
 				info = line.split(" ");
-				if(info[0].equals(userID) && info[1].equals(password)){
+				if(info[0].trim().equals(userID) && info[1].trim().equals(hash(password).trim())){
 					br.close();
 					return true;
 				}
@@ -40,4 +43,33 @@ public class ServerCheck {
 		}
 		return false;
 	}
+	
+	public String hash(String password){
+		String salt = "ECE422Project2SaltingString";
+		MessageDigest mg = null;
+		String encryptP;
+		try {
+			mg = MessageDigest.getInstance("SHA");
+			mg.update((password+salt).getBytes());
+			encryptP = (new BigInteger(mg.digest(password.getBytes()))).toString(16);
+			return encryptP;
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	public static void main(String[] args) {
+		ServerCheck se = new ServerCheck();
+		String unenct = "abcde";
+		
+		boolean a = se.readShadowFile("tom", "abcde");
+		
+		String en = se.hash(unenct);
+		System.out.println("hash: "+ en);
+	}
+	
+	
 }
